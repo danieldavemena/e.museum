28fef9e5fa4cfebe86e4a78dab03f1296ae2bfe8
isnotDave <54711946+danieldavemena@users.noreply.github.com>
2025-02-11 05:19:53 +0000

    new file:   app/components/homepage.tsx 	new file:   app/components/topbar.tsx 	new file:   public/dot-bg.png 	new file:   public/font/ChampagneAndLimousines-7KRB.ttf 	new file:   public/font/ChampagneAndLimousinesBold-myr2.ttf 	new file:   public/font/ChampagneAndLimousinesBoldItalic-dqex.ttf 	new file:   public/font/ChampagneAndLimousinesItalic-PlRZ.ttf 	new file:   public/font/champagne-and-limousines-font.zip 	new file:   public/font/fontspring-demo-27.ttf 	new file:   public/font/info.txt 	new file:   public/font/misc/Font-license-readme.txt

---
+++ app/components/homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <div >
+        <h1 className="banner-font">Tanginamo</h1>
+    </div>
+  )
+}
+
+export default homepage
---
+++ app/components/topbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+
+      <div className="topbar-font text-xl h-20 px-12 flex w-screen items-center justify-center">
+        <div className="flex text-white ml-0 mr-auto gap-8">
+          <img src="#" alt="Logo" className="logo" />
+          <nav className="flex justify-center gap-8">
+            <a href="#"><h3>Discover</h3></a>
+            <a href="#"><h3>Exhibits</h3></a>
+            <a href="#"><h3>Artists</h3></a>
+            <a href="#"><h3 className='animate'>Contribute</h3></a>
+          </nav>
+        </div>
+        <div className="flex text-white flex-row gap-3">
+          <div className="flex flex-row gap-2 px-4 py-2 bg-gray-900 rounded-3xl">
+            <h3>Item</h3>
+          </div>
+          <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
+            <h3>Item</h3>
+          </div>
+          <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
+            <h3>Item</h3>
+          </div>
+        </div>
+      </div>
+
+  )
+}
+
+export default Topbar
