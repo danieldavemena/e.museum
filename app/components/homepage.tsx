f491109696a275ef60cb717ee780a5475522df73
isnotDave <54711946+danieldavemena@users.noreply.github.com>
2025-02-11 12:16:01 +0000

    modified:   app/components/homepage.tsx 	modified:   app/globals.css

--- app/components/homepage.tsx
+++ app/components/homepage.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const homepage = () => {
   return (
-    <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 flex flex-row gap-5 '>
-        <h1 className="banner-font text-gray-500 text-9xl">Tangina</h1>
-        <h1 className="banner-font text-gray-200 text-9xl">Ka</h1>
+    <div className='w-screen overflow-x-hidden'>
+      <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 w-svw flex items-center flex-col '>
+        <div className='flex flex-row gap-5 '>
+          <h1 className="banner-font text-gray-500 text-6xl tracking-wider">Welcome to </h1>
+          <h1 className="banner-font text-gray-200 text-6xl tracking-wider">Brand:</h1>
+        </div>
+        <h1 className="banner-font text-gray-500 text-6xl tracking-wider">A Description to Brand</h1>
+      </div>
+      <div className='card-container absolute top-[67%] flex items-center w-svw flex-col'>
+        <div className="card absolute transform rotate-[8deg] translate-x-20 bg-gray-400 h-96 w-72 rounded-lg">
+          
+        </div>
+        <div className="card absolute bg-gray-300 h-96 w-72 rounded-lg">
+          
+        </div>
+        
+      </div>
     </div>
   )
 }
