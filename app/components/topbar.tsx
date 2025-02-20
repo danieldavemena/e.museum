f2286eb49ca4ff6fdaf29fd295a06312426c49cc
danieldavemena <danieldanieldanieldave@gmail.com>
2025-02-20 14:28:43 +0800

    modified:   app/components/homepage.tsx 	modified:   app/components/topbar.tsx 	new file:   app/discover/page.tsx 	new file:   app/login/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/components/topbar.tsx
+++ app/components/topbar.tsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Topbar = () => {
   return (
-
-      <div className="topbar-font text-xl h-20 px-12 flex w-screen items-center justify-center">
-        <div className="flex text-white ml-0 mr-auto gap-8">
-    
-          <nav className="flex justify-center gap-8">
-            <a href="#"><h3>Discover</h3></a>
-            <a href="#"><h3>Exhibits</h3></a>
-            <a href="#"><h3>Artists</h3></a>
-            <a href="#"><h3 className='animate'>Contribute</h3></a>
-          </nav>
-        </div>
-        <div className="flex text-white flex-row gap-3">
-          <div className="flex flex-row gap-2 px-4 py-2 bg-gray-900 rounded-3xl">
-            <h3>Item</h3>
-          </div>
-          <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
-            <h3>Item</h3>
-          </div>
-          <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
-            <h3>Item</h3>
-          </div>
-        </div>
+    <div className="topbar-font text-xl h-20 px-12 flex w-screen items-center justify-center">
+      <div className="flex text-white ml-0 mr-auto gap-8">
+        <nav className="flex justify-center gap-8">
+          <Link href="/discover">Discover</Link>
+          <a href="#">
+            <h3>Exhibits</h3>
+          </a>
+          <a href="#">
+            <h3>Artists</h3>
+          </a>
+          <a href="#">
+            <h3 className="animate">Contribute</h3>
+          </a>
+        </nav>
       </div>
+      <div className="flex text-white flex-row gap-3">
+        <div className="flex flex-row gap-2 px-4 py-2 bg-gray-900 rounded-3xl">
+          <h3>Item</h3>
+        </div>
+        <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
+          <h3>Item</h3>
+        </div>
+        <Link href="/login">
+          <div className="flex flex-row gap-2 px-4 py-2 bg-grey-800 rounded-3xl">
+            <h3>Login</h3>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Topbar
+export default Topbar;
---
+++ app/login/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="fixed m-2 top-0 right-0 h-10 w-10 bg-gray-200"></div>
+
+      <div className="topbar-font fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="banner-font text-5xl flex flex-row gap-5 items-center justify-center my-5">
+          <h1 className="  text-gray-400 ">Login to</h1>
+          <h1 className="  text-gray-200 ">E.museum</h1>
+        </div>
+
+        <div className="bg-gray-200 flex w-[500px] shadow-lg shadow-gray-800 flex-col gap-2 p-5 rounded-xl">
+          <h3 className="text-gray-700">Email:</h3>
+          <input
+            className="rounded-lg bg-gray-300 text-lg p-[5px] mb-5"
+            type="text"
+          />
+          <h3 className="text-gray-700">Password:</h3>
+          <input
+            className="rounded-lg bg-gray-300 text-lg p-[5px]"
+            type="password"
+          />
+          <button className="text-gray-700  mt-5 bg-gray-300 shadow-md shadow-gray-400 text-xl p-2">
+            Login
+          </button>
+        </div>
+
+        <div className="items-center justify-center mt-5 text-xl flex flex-row gap-3">
+          <h2 className="text-gray-400">Not a user yet?</h2>
+          <h2 className="text-gray-200">Register Now</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
