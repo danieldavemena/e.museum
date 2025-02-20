b8d3bacaf1600d62e5842e4fd0bb27bee0d1abbe
danieldavemena <danieldanieldanieldave@gmail.com>
2025-02-21 01:48:19 +0800

    modified:   app/discover/page.tsx

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Topbar from "../components/topbar";
 import supabase from "@/lib/initSupabase";
 
 const page = () => {
-  const [data, setData] = useState<{ id: string; title: string }[]>([]);
-  useEffect(() => {
+  const [data, setData] = React.useState<{ id: string; title: string }[]>([]);
+  React.useEffect(() => {
     const getData = async () => {
       const { data, error } = await supabase.from("Sample").select();
       setData(data || []);
@@ -18,7 +18,7 @@ const page = () => {
   return (
     <div>
       <Topbar />
-      <div className="columns-1 md:columns-2 lg:columns-3 p-5 [&>*]:rounded-lg [&>!first-child]:mt-3">
+      <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 p-5 [&>*]:rounded-lg [&>!first-child]:mt-3">
         {data.map((datas) => {
           return (
             <div className="bg-gray-200" key={datas.id}>
