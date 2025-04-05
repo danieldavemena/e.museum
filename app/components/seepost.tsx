19427064714fc02440cf78710296e8b0ab46afa3
danieldavemena <danieldanieldanieldave@gmail.com>
2025-04-05 23:32:14 +0800

    modified:   app/components/seepost.tsx 	modified:   app/discover/page.tsx

--- app/components/seepost.tsx
+++ app/components/seepost.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import Image from "next/image";
+import { LuArrowBigLeft } from "react-icons/lu";
 
 interface props {
   src: string;
+  closing: () => void;
 }
 
-const seepost: React.FC<props> = ({ src }) => {
+const seepost: React.FC<props> = ({ src, closing }) => {
   return (
-    <div className="fixed z-10 top-0 left-0 h-screen w-screen ">
-      <div className="size-[300px] overflow-hidden relative">
-        <Image
-          src={src}
-          alt=""
-          width={500}
-          height={500}
-          layout="intrinsic"
-          className=" object-contain"
-        />
+    <div className="fixed z-10 top-0 left-0 h-screen w-screen animate-fade-in flex flex-row items-center justify-center">
+      <div className="bg-gray-300 rounded-lg">
+        <div className="h-[600px] w-[1000px] overflow-hidden relative m-5">
+          <div
+            onClick={closing}
+            className="absolute top-0 right-0 cursor-pointer bg-red-400 rounded-lg size-max p-1"
+          >
+            <LuArrowBigLeft color="oklch(0.967 0.003 264.542)" size={30} />
+          </div>
+          <div className="flex flex-row h-full">
+            <div className="bodyBG rounded-lg w-2/3 ml-0 mr-auto flex flex-col items-center justify-center">
+              <Image
+                src={src}
+                width={0}
+                height={0}
+                unoptimized
+                className="object-contain w-auto h-full "
+                alt="uploaded_image"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import Topbar from "../components/topbar";
 import ModalElement from "../components/modalelement";
 import supabase from "@/lib/initSupabase";
-import { LuFileImage, LuArrowBigLeft, LuCirclePlus } from "react-icons/lu";
+import { LuCirclePlus } from "react-icons/lu";
 import Image from "next/image";
 import SeePost from "../components/seepost";
 
@@ -15,6 +15,8 @@ const page = () => {
   const [modal, setModal] = useState(false);
   const [blur, setBlur] = useState("");
   const [isLoading, setComplete] = useState(true);
+  const [postID, setPostID] = useState<string | null>(null);
+  const [postImage, setImage] = useState<string>("");
 
   const getData = async () => {
     const { data, error } = await supabase
@@ -48,14 +50,26 @@ const page = () => {
     setComplete(false);
   };
 
+  const closePost = () => {
+    setPostID(null);
+
+    console.log(postID);
+  };
+
   return (
     <div>
       <div className={`${blur} transition duration-150 ease-in-out`}>
         <Topbar />
-        <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 p-5 [&>*]:rounded-lg [&>:not(:first-child)]:mt-3">
+        <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 p-10 [&>*]:rounded-lg [&>:not(:first-child)]:mt-4">
           {data.map((datas) => {
             return (
-              <div className="text-gray-300 flex flex-col gap-4" key={datas.id}>
+              <div
+                className="text-gray-300 flex flex-col cursor-pointer gap-4 hover:scale-[1.02] hover:shadow-[0_0_10px_#ffffff] transition-all duration-150 ease-in-out"
+                key={datas.id}
+                onClick={() => {
+                  setImage(datas.image);
+                }}
+              >
                 <Image
                   src={datas.image}
                   width={500}
@@ -77,8 +91,9 @@ const page = () => {
         <LuCirclePlus color="oklch(0.552 0.016 285.938)" size={50} />
       </div>
       {/* Modal to see Post */}
-      {/* <SeePost src="/devImages/profile.jpg" /> */}
-
+      {postImage != "" && (
+        <SeePost src={postImage} closing={() => setImage("")} />
+      )}
       {modal && <ModalElement closing={postModal} update={getData} />}
     </div>
   );
